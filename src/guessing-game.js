1fecf2f11575fec99eae1026551212cae4579cb5
Your Name <you@example.com>
2019-10-13 10:34:01 +0300

    find solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        
+    }
 
     setRange(min, max) {
-
+       this.min=min;
+         this.max=max;
+       
     }
 
     guess() {
-
+      
+        this.mid= Math.ceil((this.min + this.max)/2);
+        
+    return this.mid
     }
 
     lower() {
-
+        this.max=this.mid;
     }
 
     greater() {
-
+     this.min=this.mid;
     }
+   
 }
+
 
 module.exports = GuessingGame;
